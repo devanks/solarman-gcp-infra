f19f4e58f08639df53e8af1e4d37ef1deb29867b
Devan K Sambasivan <devansamba96@gmail.com>
2025-05-23 04:49:41 +0530

    Remove functions/archiver-java in preparation for submodule

--- functions/archiver-java/src/main/java/dev/devanks/solarman/archiver/repository/SolarReadingHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.devanks.solarman.archiver.repository;
-
-import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
-import dev.devanks.solarman.archiver.entity.SolarReadingHistoryEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SolarReadingHistoryRepository extends FirestoreReactiveRepository<SolarReadingHistoryEntity> {
-}
